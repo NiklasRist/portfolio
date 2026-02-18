34020ac7fd7fb6405c711c33dd31b7873da1d55f
Niklas <niklas.a.rist@gmail.com>
2026-02-18 16:18:47 +0100

    + collapsable header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,29 +1,58 @@
-import {styles} from "./header.styles";
-import {NavLink, type NavLinkRenderProps} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { styles } from "./header.styles";
+import { NavLink, type NavLinkRenderProps } from "react-router-dom";
 
-export function Header(){
+export function Header() {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollY = window.scrollY;
+
+            if (!isCollapsed && scrollY > 220) {
+                setIsCollapsed(true);
+            }
+
+            if (isCollapsed && scrollY < 120) {
+                setIsCollapsed(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [isCollapsed]);
+
+
     const links = [
-        {label: "Projekte", to: "/#projekte"},
-        {label: "Kontakt", to: "/#kontakt"},
-        {label: "Über mich", to: "/#ueber-mich"},
+        { label: "Projekte", to: "/#projekte" },
+        { label: "Kontakt", to: "/#kontakt" },
+        { label: "Über mich", to: "/#ueber-mich" },
     ];
+
     return (
         <header style={styles.header}>
-            <h1 style={styles.title}>
-                Niklas <br/>
-                Rist
-            </h1>
-            <aside style={styles.newsfeed}>
-                Breaking News! <br/>
-                Neueste Nachrichten berichtet im Nachrichtenfeed!!!!
-            </aside>
+
+            {!isCollapsed && (
+                <>
+                    <h1 style={styles.title}>
+                        Niklas <br />
+                        Rist
+                    </h1>
+
+                    <aside style={styles.newsfeed}>
+                        Breaking News! <br />
+                        Neueste Nachrichten berichtet im Nachrichtenfeed!!!!
+                    </aside>
+                </>
+            )}
+
             <nav style={styles.nav}>
                 {links.map(({ label, to }) => (
                     <NavLink
                         key={to}
                         to={to}
                         end={to === "/"}
-                        style={({ isActive } :NavLinkRenderProps) =>
+                        style={({ isActive }: NavLinkRenderProps) =>
                             isActive
                                 ? { ...styles.navLink, ...styles.navLinkActive }
                                 : styles.navLink
@@ -36,4 +65,3 @@ export function Header(){
         </header>
     );
 }
-
