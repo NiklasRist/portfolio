922a472cc086b3e68d27f50d747bfd071a6562b5
Niklas <niklas.a.rist@gmail.com>
2026-02-18 11:52:18 +0100

    + setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: 'https://niklasrist.github.io/portfolio/',
   plugins: [react()],
 })
